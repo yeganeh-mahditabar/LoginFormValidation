4a779d90b1c49829d7e4f460b38b8a4481e38868
yeganeh-mahditabar <yeganehhhhh.2001@gmail.com>
2024-09-14 01:31:45 +0430

    login condition

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ let passwordInput = document.querySelector('.password');
 function dataValidation() {
     let userNameValue = userNameInput.value;
     let passwordValue = passwordInput.value;
+    
+    if(userNameValue.length < 12 || passwordValue.length < 8) {
+        modal.style.display = 'inline';
+
+        setTimeout(function() {
+            modal.style.display = 'none';
+        }, 3000);
+    } else {
+        modal.style.background = 'green';
+        modal.innerHTML = 'لاگین با موفقیت انجام شد';
+        modal.style.display = 'inline';
+    }
 }
