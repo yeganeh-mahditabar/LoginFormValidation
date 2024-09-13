1725a5987bbf524844b81385f397f4673fd879a7
yeganeh-mahditabar <yeganehhhhh.2001@gmail.com>
2024-09-14 01:04:12 +0430

    fun dataValidation

---
+++ script.js
@@ -0,0 +1,7 @@
+let userNameInput = document.querySelector('.username');
+let passwordInput = document.querySelector('.password');
+
+function dataValidation() {
+    let userNameValue = userNameInput.value;
+    let passwordValue = passwordInput.value;
+}
