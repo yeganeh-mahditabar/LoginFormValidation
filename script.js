1236568f6e383361dcf965a4ad961488e5c60a43
yeganeh-mahditabar <yeganehhhhh.2001@gmail.com>
2024-09-14 01:51:33 +0430

    update function

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let userNameInput = document.querySelector('.username');
 let passwordInput = document.querySelector('.password');
+let modal = document.querySelector('.modal');
 
 function dataValidation() {
     let userNameValue = userNameInput.value;
     let passwordValue = passwordInput.value;
     
     if(userNameValue.length < 12 || passwordValue.length < 8) {
+        modal.innerHTML = 'لطفا اطلاعات لازم را به درستی وارد کنید';
+        modal.style.background = 'rgb(223, 28, 28)';
         modal.style.display = 'inline';
-
-        setTimeout(function() {
-            modal.style.display = 'none';
-        }, 3000);
     } else {
         modal.style.background = 'green';
         modal.innerHTML = 'لاگین با موفقیت انجام شد';
         modal.style.display = 'inline';
     }
+    
+    setTimeout(function() {
+        modal.style.display = 'none';
+    }, 3000);
 }
